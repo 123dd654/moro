58b416065a2d3d74706993f146f3da0c8228fa72
123dd654 <161431124+123dd654@users.noreply.github.com>
2026-03-18 17:32:58 +0900

    메인화면 퍼블리싱

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom";
+import "./header.css";
+import logo from "../assets/logo.png";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div className="Header_left">
+        <Link to="/">
+          <img src={logo} alt="" />
+        </Link>
+      </div>
+      <nav className="Header_right">
+        <ul>
+          <li>계산기</li>
+          <li>질문</li>
+          <li>소통</li>
+          <li>일정</li>
+          <li>로그인</li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,47 @@
 import { Link } from "react-router-dom";
+import "./Home.css";
 
 const Home = () => {
   return (
     <section>
       {/* 소개 */}
-      <div>
-        <h1>세무를 쉽게, 한 번에 확인하세요</h1>
-        <p>복잡한 세무 계산을 빠르게 해결할 수 있습니다.</p>
+      <div className="introduction">
+        <h1 className="introduction_top">
+          <span className="top_color">MORO</span>가 당신만을 위한
+          <br />
+          똑똑한 가이드가
+          <br />
+          되어드릴게요!
+        </h1>
+        <p className="introduction_middle">
+          <span className="middle_color">
+            실수령액 역계산 · 4대보험 · 주휴수당
+          </span>
+          을
+          <br />
+          가장 빠르고 간편하게 계산해보세요
+        </p>
 
         {/* 버튼 */}
         <Link to="/calculator">
-          <button>계산기 바로가기</button>
+          <div className="introduction_bottom">
+            <button className="main_btn">계산기 시작하기</button>
+          </div>
         </Link>
       </div>
 
       {/* 아래 섹션 */}
-      <div>
-        <h2>사업자등록조회</h2>
-        <p>사업자 상태를 확인해보세요</p>
+      <div className="business">
+        <div className="business_title">
+          <h2>사업자등록조회</h2>
+          <p>사업자 정보를 빠르게 확인하고 진위 여부를 파악할 수 있습니다</p>
+        </div>
+        <div className="business_imput"></div>
       </div>
 
       <div>
         <h2>세무 도서 추천</h2>
-        <p>입문자를 위한 도서</p>
+        <p>실무에 도움이 되는 도서를 소개합니다</p>
       </div>
     </section>
   );
